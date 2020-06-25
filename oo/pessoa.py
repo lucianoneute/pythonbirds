4b4bdcb4948107a6c2ffe1b560ca8de462057fb6
Luciano Neute <luciano.neute@gmail.com>
2020-06-25 09:10:15 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,22 @@
 class Pessoa:
+    #declarar metodo com atributos
+    def __init__(self, nome=None, idade = 42):
+        #atributo self.nome
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
+    #mostrar mensagem e id
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    #imprime valor do atributo
+    print(p.nome)
+    #alterar valor do atributo
+    p.nome = 'LucianoNeute'
+    print(p.nome)
+    print(p.idade)
